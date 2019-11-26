999c1c5d18102d17fc09fe752cc42758501a16a0
Abeku Cobbina <acobbina717@gmail.com>
2019-11-26 09:39:45 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,3 +1,3 @@
-def greeting(persons_name)
-  puts "Hello #{persons_name} It's nice to meet you."
+def greeting(person_name)
+    puts "Hello #{person_name} It's nice to meet you."
 end# code the #greeting method here!
